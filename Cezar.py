878029e637b77ed33a2e9a1d1e31f73cfc24b66b
Vlad Taranov <taranov.vlad.1995@gmail.com>
2018-12-17 18:18:50 +0200

    Add checker specified symbols

--- Cezar.py
+++ Cezar.py
@@ -2,11 +2,20 @@ alp = 'abcdefghijklmnopqrstuvwxyz'
 input_string = input("Введите текст для зашифрования:")
 input_step = int(input("Шаг:"))
 result = ''
+check = False
 
-for c in input_string:
-    if alp.index(c) + input_step < len(alp):
-        result += alp[alp.index(c) + input_step]
+
+for letter_in_input_string in input_string:
+    for letter_in_alp in alp:
+        if letter_in_input_string == letter_in_alp:
+            check = True
+
+    if check == True:
+        if alp.index(letter_in_input_string) + input_step < len(alp):
+            result += alp[alp.index(letter_in_input_string) + input_step]
+        else:
+            result += alp[alp.index(letter_in_input_string) + input_step - len(alp)]
+        check = False
     else:
-        result += alp[alp.index(c) + input_step - len(alp)]
-
-print('Result: ' + result)
+        result += letter_in_input_string
+print('Result:' + result)
